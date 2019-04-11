7f2f989b5612094161f34c55be32947fcfc20015
Emily Mattison <egmatti@gmail.com>
2019-04-10 23:12:27 -0400

    First commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// GETTING STARTED EXAMPLE
+// $(document).ready(function(){
+//   // jQuery code
+// });
+
+
+// MEDIA QUERIES
+
+const mediaQuerySmall = window.matchMedia( "(max-width: 767px)" );
+const mediaQueryMedium = window.matchMedia( "(max-width: 1025px)" );
